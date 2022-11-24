48eb8ab089abb8de07a636d40a4183324d503b6f
Amit Roy <amit.akr.roy@gmail.com>
2022-11-24 10:57:18 +0100

    Removed comments
    Removed comments.

--- edit-distance-table.cpp
+++ edit-distance-table.cpp
@@ -12,15 +12,13 @@ int main(){
 	cout<<"String Y: \n";
 	cin>>y;
 	int m=y.length();
-	//x=amit , y=timax  , m=5, n=4 j<=4, i<=5
 	vector<vector<int> > ED(m+1,vector<int>(n+1,0));
-	vector<vector<char> > align(m+1,vector<char>(n+1,' '));
-	//cout<<m<<" "<<n<<endl;
+	vector<vector<char> > align(m+1,vector<char>(n+1,' ')); //Alignment Vector
 	for(int i=0;i<=m;i++){
-		for(int j=0;j<=n;j++){ 
+		for(int j=0;j<=n;j++){
 			if(i==0){
 				ED[i][j]=j;
-				align[i][j]='L';
+				align[i][j]='L'; //Alignment
 			}
 			else if(j==0){
 				ED[i][j]=i;
@@ -39,26 +37,21 @@ int main(){
 			}
 			else{
 				ED[i][j]=min(1+min(ED[i][j-1],ED[i-1][j]) , 1+ ED[i-1][j-1]);
-				if(min(ED[i][j-1],ED[i-1][j]) > ED[i-1][j-1])
-					align[i][j]='D';
-				else{
+				if(min(ED[i][j-1],ED[i-1][j]) < ED[i-1][j-1]){
 					if(ED[i][j-1] > ED[i-1][j])
 						align[i][j]='L';
 					else
 						align[i][j]='B';
 				}
+				else{
+					align[i][j]='D';
+				}
 			}
-			
 		}
 	}
+
 	// PRINT TABLE WITH ALIGNMENT
 	for(int i=m;i>=0;i--){
-		/*if(i>0)
-			cout<<y[i-1]<<" ";
-		else
-			cout<<"  ";*/
-		//string direction=align[i][j];
-		
 		for(int j=0;j<=n;j++){
 			cout
 			<<left
@@ -68,17 +61,6 @@ int main(){
 		}
 		cout<<endl;
 	}
-
-	/*cout<<"   ";
-	for(int i=0 ; i<x.length();i++)
-		cout<<" "<<x[i];
-	cout<<"\n\n";
-	for(int i=m;i>=0;i--){
-		for(int j=0;j<=n;j++){
-			cout<<align[i][j]<<" ";
-		}
-		cout<<endl;
-	}*/
 
 	return 0;
 }
